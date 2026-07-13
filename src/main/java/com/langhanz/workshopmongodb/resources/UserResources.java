34e43ecc461053b37c1ac9fd5b0f20f11c4b264c
João Langhanz <joao.langhanz@holdingffh.com.br>
2026-07-13 19:16:16 -0300

    Exceptions handlers

--- src/main/java/com/langhanz/workshopmongodb/resources/UserResources.java
+++ src/main/java/com/langhanz/workshopmongodb/resources/UserResources.java
@@ -6,12 +6,14 @@ import com.langhanz.workshopmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -27,5 +29,15 @@ public class UserResources {
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @GetMapping
+    @RequestMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+
+        User user = service.findById(id);
+
+        return ResponseEntity.ok().body(new UserDTO(user));
+
     }
 }
--- src/main/java/com/langhanz/workshopmongodb/services/UserService.java
+++ src/main/java/com/langhanz/workshopmongodb/services/UserService.java
@@ -2,10 +2,12 @@ package com.langhanz.workshopmongodb.services;
 
 import com.langhanz.workshopmongodb.domain.User;
 import com.langhanz.workshopmongodb.repository.UserRepository;
+import com.langhanz.workshopmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,6 +18,12 @@ public class UserService {
     public List<User> findAll(){
 
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user = repo.findById(id);
+
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
 
     }
 
